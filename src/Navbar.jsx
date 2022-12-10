e5131cfe507e88dd3e6744db1e3ab255e7cf0c46
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-12-10 22:22:33 +0530

    tesla clone

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='nav'>
+        <div className='nav_logo'>
+           <Link to=''>
+            
+           </Link>
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
