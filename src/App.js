ed1e8d156bd114a8dbe16ce2c8cf2cd9693281e3
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-12-10 23:41:37 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import { BrowserRouter as Router,Routes } from 'react-router-dom';
+import { useState } from 'react';
 function App() {
+  const [isSideMenu,setSideMenuOpen] = useState(false);
   return (
     <div className="App">
-        <p>Hello first!!!!!!!!!
-        </p>
+      <Router>
+      <>
+        {/* <p>Hello first!!!!!!!!!
+        </p> */}
+        <Navbar isSideMenu={isSideMenu} setSideMenuOpen={setSideMenuOpen}/>
+        </>
+        </Router>
     </div>
   );
 }
